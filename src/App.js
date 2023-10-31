c0e1eaa578432e12220e816fb3e3728008c35d8e
developedbyWill <127629003+developedbyWill@users.noreply.github.com>
2023-10-31 22:36:26 +0400

    initialized 'react-counter', added ClassCounter with count state and events.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ClassCounter from "./components/ClassCounter";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Class Counter</h2>
+      <ClassCounter />
+    </div>
+  );
+}
+
+export default App;
